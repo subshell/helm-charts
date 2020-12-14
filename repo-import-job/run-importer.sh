dc622f7459c4a7209f0aa2dd3deba2654562191e
Tom Schoener <schoener@subshell.com>
2020-12-14 11:47:52 +0100

    Repo Import Helm Chart scripts

---
+++ repo-import-job/run-importer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+count_files() {
+  result=$(find $1 -type f | wc -l)
+  echo "$result"
+}
+
+cd /sophora || exit
+echo "Starting importer."
+java -cp classpath org.springframework.boot.loader.JarLauncher
+echo "Importer stopped."
+
+success=$(count_files /import/admin/success)
+failure=$(count_files /import/admin/failure)
+
+printf "successful imports: %i\t import failures: %i\n" "$success" "$failure"
+
+if [ "$failure" -gt 0 ]; then
+  # error
+  echo "import was not successful"
+  exit 1
+else
+  echo "import was successful"
+  exit 0
+fi
+
---
+++ repo-import-job/wait-for-empty-dir.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+count_files() {
+  count=$(find /import/admin/incoming | grep ".xml" | wc -l)
+  echo $count
+}
+
+currentFileCount=$(count_files)
+while [ $currentFileCount -gt 0 ]
+do
+  echo "Still waiting. Current file count is ${currentFileCount}"
+  sleep 1
+  currentFileCount=$(count_files)
+done
+
+echo "No files remaining. We wait another 30 seconds."
+sleep 30
+# Kill the importer:
+importerPid=$(pgrep java)
+kill $importerPid
