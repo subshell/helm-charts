688a2e18c94b3293f004c6b630db002694ff821f
Melwin Kramer <59892006+Melwiin@users.noreply.github.com>
2025-09-11 16:44:58 +0200

    [sophora-export-job] added non zip export functionality (#210)
    * feat: added non zip export functionality
    
    * fix: changed artifact change description appropriately
    
    ---------
    
    Co-authored-by: Melwin Kramer <kramer@subshell.com>

--- charts/sophora-export-job/files/upload-to-s3.sh
+++ charts/sophora-export-job/files/upload-to-s3.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
 # see https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html
-# this script requires env variables S3_NAME, S3_FILE_PATH_WITHOUT_EXTENSION, S3_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and STORE_UPLOAD_PER_WEEKDAY to be set
-
-echo "creating zip"
-tree /data-export
-cd /data-export || exit
-mkdir "/output"
-zip -q9rD "/output/export.zip" .
+# this script requires env variables S3_NAME, S3_FILE_PATH_WITHOUT_EXTENSION, S3_ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, USE_ZIP to be set
 
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/s3/mv.html
 echo "start uploading to s3..."
 uploadStart=$(date +%s%3N)
-aws --endpoint="$S3_ENDPOINT" s3 cp "/output/export.zip" "s3://$S3_NAME$S3_FILE_PATH_WITHOUT_EXTENSION.zip"
+
+if [ "$USE_ZIP" = "true" ]; then
+  tree /data-export
+  cd /data-export || exit
+  mkdir "/output"
+
+  echo "creating zip file for upload"
+  zip -q9rD "/output/export.zip" .
+  aws --endpoint="$S3_ENDPOINT" s3 cp "/output/export.zip" "s3://$S3_NAME$S3_FILE_PATH_WITHOUT_EXTENSION.zip"
+else
+  tree /data-export/documents
+  cd /data-export/documents || exit
+  mkdir "/output"
+
+  echo "uploading individual files"
+  aws --endpoint="$S3_ENDPOINT" s3 cp . "s3://$S3_NAME$S3_FILE_PATH_WITHOUT_EXTENSION/" --recursive
+fi
 
 uploadEnd=$(date +%s%3N)
 uploadDurationMillis=$((uploadEnd-uploadStart))
